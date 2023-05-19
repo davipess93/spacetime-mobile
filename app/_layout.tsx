066760c65e1eec53fc2dc3c1504f756298b7e8d2
Davi Pessoa <davialvespessoa@gmail.com>
2023-05-19 13:24:44 -0300

    Inital commit

---
+++ app/_layout.tsx
@@ -0,0 +1,61 @@
+import { styled } from 'nativewind'
+import { ImageBackground } from 'react-native'
+import { Stack, SplashScreen } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import * as SecureStore from 'expo-secure-store'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import { useEffect, useState } from 'react'
+
+const StyledStrips = styled(Stripes)
+
+export default function Layout() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState<boolean>(null)
+
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) => {
+      setIsUserAuthenticated(!!token)
+    })
+  }, [])
+
+  if (!hasLoadedFonts) {
+    return <SplashScreen />
+  }
+
+  return (
+    <ImageBackground
+      source={blurBg}
+      className="items relative flex-1 bg-zinc-900"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStrips className="absolute left-2" />
+      <StatusBar style="light" translucent />
+
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: 'transparent' },
+        }}
+      >
+        <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="new" />
+        <Stack.Screen name="memories" />
+      </Stack>
+    </ImageBackground>
+  )
+}
